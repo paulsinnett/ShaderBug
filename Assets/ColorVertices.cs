16e0041c20983485c0769248bc365c575e9f56e5
Paul Sinnett <paul.sinnett@gmail.com>
2019-04-12 18:24:12 +0100

    Initial commit

---
+++ Assets/ColorVertices.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorVertices : MonoBehaviour
+{
+	void Start()
+	{
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		Color[] colors = new Color[mesh.vertices.Length];
+		for (int i = 0; i < colors.Length; ++i)
+		{
+			colors[i] = new Color(1.0f, 0.5f, 0.1f, 1.0f);
+		}
+		mesh.colors = colors;
+	}
+}
